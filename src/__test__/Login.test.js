b569200854df51b5d5884a05335999d72b582bb4
heerthu98 <98heerthu@gmail.com>
2023-06-15 14:03:40 +0530

    validation | responsive

--- src/__test__/Login.test.js
+++ src/__test__/Login.test.js
@@ -1,4 +1,9 @@
-import { render as rt1Render, screen } from "@testing-library/react";
+import {
+  render as rt1Render,
+  screen,
+  fireEvent,
+  act,
+} from "@testing-library/react";
 import { Provider } from "react-redux";
 import store from "../App/store";
 import Login from "../components/Login";
@@ -23,5 +28,54 @@ describe("Test the Login component", () => {
     render(<Login />);
     const email = screen.getByPlaceholderText("Email");
     expect(email).toHaveAttribute("type", "email");
+  });
+
+  test("submitting the form without email should display an error message", () => {
+    render(<Login />);
+
+    const submitButton = screen.getByTestId("submit");
+
+    fireEvent.click(submitButton);
+
+    const emailError = screen.getByText("Email is required");
+    expect(emailError).toBeInTheDocument();
+  });
+
+  test("submitting the form with an invalid email format should display an error message", () => {
+    render(<Login />);
+
+    const emailInput = screen.getByPlaceholderText("Email");
+    const submitButton = screen.getByTestId("submit");
+
+    fireEvent.change(emailInput, { target: { value: "example" } });
+    fireEvent.click(submitButton);
+
+    const emailError = screen.getByText("Invalid email format");
+    expect(emailError).toBeInTheDocument();
+  });
+
+  test("submitting the form without a password should display an error message", () => {
+    render(<Login />);
+
+    const submitButton = screen.getByTestId("submit");
+
+    fireEvent.click(submitButton);
+
+    const passwordError = screen.getByText("Password is required");
+    expect(passwordError).toBeInTheDocument();
+  });
+
+  test("submitting the form with an invalid password format should display an error message", () => {
+    render(<Login />);
+
+    const passwordInput = screen.getByPlaceholderText("Password");
+    const submitButton = screen.getByTestId("submit");
+
+    fireEvent.change(passwordInput, { target: { value: "password123" } });
+    fireEvent.click(submitButton);
+    const passwordError = screen.getByText(
+      "Password must contain at least 8 characters including one letter, one number and one special character"
+    );
+    expect(passwordError).toBeInTheDocument();
   });
 });
--- src/components/Login.js
+++ src/components/Login.js
@@ -6,24 +6,56 @@ import { login } from "../features/userSlice";
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
 
   const dispatch = useDispatch();
 
   function handleLogin(e) {
     e.preventDefault();
 
-    dispatch(
-      login({
-        email: email,
-        password: password,
-        loggedIn: true,
-      })
-    );
+    let isValid = true;
+
+    if (!email) {
+      setEmailError("Email is required");
+      isValid = false;
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      setEmailError("Invalid email format");
+      isValid = false;
+    } else {
+      setEmailError("");
+    }
+
+    if (!password) {
+      setPasswordError("Password is required");
+      isValid = false;
+    } else if (
+      !/"(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(
+        password
+      )
+    ) {
+      setPasswordError(
+        "Password must contain at least 8 characters including one letter, one number and one special character"
+      );
+      isValid = false;
+    } else {
+      setPasswordError("");
+    }
+
+    if (isValid) {
+      dispatch(
+        login({
+          email: email,
+          password: password,
+          loggedIn: true,
+        })
+      );
+    }
   }
 
   return (
     <div className="login__container container">
-      <form className="login__form" onSubmit={(e) => handleLogin(e)}>
+      <form className="login__form" onSubmit={(e) => handleLogin(e)} noValidate>
         <div className="Login__profile">
           <BsFillPersonFill size={90} />
         </div>
@@ -34,12 +66,16 @@ function Login() {
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
+        {emailError && <p className="error">{emailError}</p>}
+
         <input
           type="password"
           placeholder="Password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
+        {passwordError && <p className="error">{passwordError}</p>}
+
         <div className="login__forgotPwd">
           <div className="login__remmberMe">
             <input type="checkbox" />
